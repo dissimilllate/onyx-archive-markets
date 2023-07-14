914f999d73c1b2f1e2e69db1f8d551078eef96e7
dissimilllate <dissimilllate@gmail.com>
2023-07-14 18:42:30 +0300

    chore: cover in try...catch

--- index.js
+++ index.js
@@ -5,37 +5,41 @@ const { CronJob } = require('cron');
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-
 async function main() {
-  await client.connect();
+  try {
+    await client.connect();
 
-  const db = client.db(process.env.MONGODB_DATABASE);
+    const db = client.db(process.env.MONGODB_DATABASE);
 
-  if (!(await db.listCollections({name: process.env.MONGODB_COLLECTION_NAME}).toArray()).length) {
-    await db.createCollection(
-      process.env.MONGODB_COLLECTION_NAME,
-      {
-        timeseries: {
-          timeField: 'timestamp',
-          metaField: 'metadata',
-          granularity: 'hours',
+    if (!(await db.listCollections({name: process.env.MONGODB_COLLECTION_NAME}).toArray()).length) {
+      await db.createCollection(
+        process.env.MONGODB_COLLECTION_NAME,
+        {
+          timeseries: {
+            timeField: 'timestamp',
+            metaField: 'metadata',
+            granularity: 'hours',
+          },
         },
-      },
-    );
-  }
+      );
+    }
 
-  const job = new CronJob(process.env.CRON_ARCHIVE_INTERVAL, async () => {
-    const data = await axios.get(process.env.ONYX_MARKETS_URL);
-    await db.collection(process.env.MONGODB_COLLECTION_NAME).insertOne({
-      timestamp: new Date(),
-      metadata: {
-        sourceUrl: process.env.ONYX_MARKETS_URL,
-      },
-      data: data.data,
+    const job = new CronJob(process.env.CRON_ARCHIVE_INTERVAL, async () => {
+      const data = await axios.get(process.env.ONYX_MARKETS_URL);
+      await db.collection(process.env.MONGODB_COLLECTION_NAME).insertOne({
+        timestamp: new Date(),
+        metadata: {
+          sourceUrl: process.env.ONYX_MARKETS_URL,
+        },
+        data: data.data,
+      });
     });
-  });
 
-  job.start();
+    job.start();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 main();
