1fa19d94331afcb68b2c8d017acca478436d6d05
dissimilllate <dissimilllate@gmail.com>
2023-07-16 17:45:58 +0300

    chore: separate db initializer

---
+++ database/Mongo.database.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+async function initDatabase() {
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI);
+
+    const db = client.db(process.env.MONGODB_DATABASE);
+
+    if (!(await db.listCollections({name: process.env.MONGODB_COLLECTION_NAME}).toArray()).length) {
+        await db.createCollection(
+          process.env.MONGODB_COLLECTION_NAME,
+          {
+            timeseries: {
+              timeField: 'timestamp',
+            metaField: 'metadata',
+            granularity: 'hours',
+          },
+        },
+      );
+    }
+
+    return db;
+  } catch (error) {
+    console.error("DB initialization error", error);
+    process.exit(1);
+  }
+}
+
+module.exports = { initDatabase };
--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 require('dotenv').config();
 const axios = require('axios');
-const { MongoClient } = require('mongodb');
 const { CronJob } = require('cron');
-
-const client = new MongoClient(process.env.MONGODB_URI);
+const { initDatabase } = require('./database/Mongo.database');
 
 async function main() {
   try {
-    await client.connect();
-
-    const db = client.db(process.env.MONGODB_DATABASE);
-
-    if (!(await db.listCollections({name: process.env.MONGODB_COLLECTION_NAME}).toArray()).length) {
-      await db.createCollection(
-        process.env.MONGODB_COLLECTION_NAME,
-        {
-          timeseries: {
-            timeField: 'timestamp',
-            metaField: 'metadata',
-            granularity: 'hours',
-          },
-        },
-      );
-    }
+    const db = await initDatabase();
 
     const job = new CronJob(process.env.CRON_ARCHIVE_INTERVAL, async () => {
       const data = await axios.get(process.env.ONYX_MARKETS_URL);
